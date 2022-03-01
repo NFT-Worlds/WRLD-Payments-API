713702cf8d7358f466aab08a3e9ee779e10109e9
Jackson Roberts <jacksonroberts25@gmail.com>
2022-02-28 21:49:35 -0700

    Warn users and fail to start if polygon_https_rpc and ethereum_https_rpc are not set.

--- src/main/java/com/nftworlds/wallet/config/Config.java
+++ src/main/java/com/nftworlds/wallet/config/Config.java
@@ -33,6 +33,16 @@ public class Config {
         this.polygonHttpsRpc = config.getString("polygon_https_rpc");
         this.ethereumHttpsRpc = config.getString("ethereum_https_rpc");
 
+        if (this.polygonHttpsRpc.isEmpty() || this.ethereumHttpsRpc.isEmpty()) {
+            Bukkit.getLogger().log(Level.SEVERE, "polygon_https_rpc and ethereum_https_rpc are not set! Please " +
+                    "set an HTTPS endpoint for an Ethereum and Polygon node. We recommend using Alchemy or Infura which" +
+                    " will allow you to get started in five minutes!");
+            Bukkit.getLogger().log(Level.SEVERE, "Shutting down server. You must configure polygon_https_rpc and " +
+                    "ethereum_https_rpc to use WRLD-Payments-API. Please see docs at " +
+                    "https://dev.nftworlds.com/payments/wrld-payments-api#configuring-ethereum-and-polygon-rpc-endpoints");
+            System.exit(-1);
+        }
+
         String serverWalletPrivateKey = config.getString("server_wallet_private_key");
         if (serverWalletPrivateKey != null && !serverWalletPrivateKey.equals("")) {
             Bukkit.getLogger().warning("A private key has been set in the plugin config! Only install " +
